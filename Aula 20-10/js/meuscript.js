fe0d03204c06ef59d44a36872bd42e2da53bbd4e
Luizhenriqueup <luiz.up.work@gmail.com>
2025-10-20 21:41:18 -0300

    Add files via upload

---
+++ Aula 20-10/js/meuscript.js
@@ -0,0 +1,12 @@
+function exibir(){
+    document.getElementById('texto').innerHTML = 'Seu mouse está sobre o texto';
+}
+function sumiu(){
+    document.getElementById('texto').innerHTML = ''
+}
+
+
+// Crie um nova seção no html
+/*
+desenvolva um botão que ao receber um clique mude a cor de um ou mais elementos
+*/
